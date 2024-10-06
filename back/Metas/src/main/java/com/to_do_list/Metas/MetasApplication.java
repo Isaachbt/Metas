a3f70891360c90b54438cb21bcb5343ffc76cb17
Isaachbt <isaac.silva1478@gmail.com>
2024-10-06 19:57:30 -0300

    Initial commit

---
+++ back/Metas/src/main/java/com/to_do_list/Metas/MetasApplication.java
@@ -0,0 +1,13 @@
+package com.to_do_list.Metas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetasApplication.class, args);
+	}
+
+}
