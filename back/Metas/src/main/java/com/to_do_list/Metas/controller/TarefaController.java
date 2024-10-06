05a4cf4f019074dccccff5106d05a0ddc50ef875
Isaachbt <isaac.silva1478@gmail.com>
2024-10-06 20:08:56 -0300

    class TarefaController criada

---
+++ back/Metas/src/main/java/com/to_do_list/Metas/controller/TarefaController.java
@@ -0,0 +1,10 @@
+package com.to_do_list.Metas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/tarefas")
+public class TarefaController {
+}
