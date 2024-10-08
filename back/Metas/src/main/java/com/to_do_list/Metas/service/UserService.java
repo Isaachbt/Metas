169f954baa65cd224a68347d6099a9bd221e679d
Isaachbt <isaac.silva1478@gmail.com>
2024-10-08 18:33:53 -0300

    interface UserService criado e metodo saveUser

---
+++ back/Metas/src/main/java/com/to_do_list/Metas/service/UserService.java
@@ -0,0 +1,9 @@
+package com.to_do_list.Metas.service;
+
+import com.to_do_list.Metas.model.User;
+import com.to_do_list.Metas.model.dto.UserDto;
+
+public interface UserService {
+
+    UserDto saveUser(UserDto dto);
+}
