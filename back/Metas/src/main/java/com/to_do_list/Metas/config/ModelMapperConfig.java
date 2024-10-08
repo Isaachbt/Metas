fc0d32d2f8d99324cf3469a59a5f34159f3b6b52
Isaachbt <isaac.silva1478@gmail.com>
2024-10-08 17:39:48 -0300

    arquivo config, config ModelMapper e ConfigLocal

---
+++ back/Metas/src/main/java/com/to_do_list/Metas/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.to_do_list.Metas.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+
+ @Bean
+    public ModelMapper modelMapper(){
+     return new ModelMapper();
+ }
+}
