a6877be1717f0767e5721f5b9d7d9a4b97db73f7
Isaachbt <isaac.silva1478@gmail.com>
2024-10-08 18:34:15 -0300

    Class UserServiceImpl criado e metodo saveUser

---
+++ back/Metas/src/main/java/com/to_do_list/Metas/service/impl/UserServiceimpl.java
@@ -0,0 +1,19 @@
+package com.to_do_list.Metas.service.impl;
+
+import com.to_do_list.Metas.model.dto.UserDto;
+import com.to_do_list.Metas.repositorio.UserRepository;
+import com.to_do_list.Metas.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceimpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDto saveUser(UserDto dto) {
+        return null;
+    }
+}
