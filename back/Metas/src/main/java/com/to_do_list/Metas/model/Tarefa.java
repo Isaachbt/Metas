eeb3368b33174de67568729a962bfc6e97f07866
Isaachbt <isaac.silva1478@gmail.com>
2024-10-08 17:40:22 -0300

    Add new construtor

--- back/Metas/src/main/java/com/to_do_list/Metas/model/Tarefa.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/model/Tarefa.java
@@ -3,13 +3,14 @@ package com.to_do_list.Metas.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_tarefas")
 public class Tarefa {
@@ -24,4 +25,11 @@ public class Tarefa {
     private LocalDateTime dataFinal;
     private Integer qDiasCompletados;
 
+    public Tarefa(Integer id, String nome, LocalDateTime dataIniciado, LocalDateTime dataFinal, Integer qDiasCompletados) {
+        this.id = id;
+        this.nome = nome;
+        this.dataIniciado = dataIniciado;
+        this.dataFinal = dataFinal;
+        this.qDiasCompletados = qDiasCompletados;
+    }
 }
