bd78c9998caeb8e81b1b41edb7f23467003e8548
Isaachbt <isaac.silva1478@gmail.com>
2024-10-08 18:59:34 -0300

    Metodo findByIdUser criado e implementado

--- back/Metas/src/main/java/com/to_do_list/Metas/service/TarefaService.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/service/TarefaService.java
@@ -1,10 +1,12 @@
 package com.to_do_list.Metas.service;
 
 import com.to_do_list.Metas.model.Tarefa;
+import com.to_do_list.Metas.model.User;
 import com.to_do_list.Metas.model.dto.TarefaDto;
 
 import java.util.List;
 
 public interface TarefaService {
     List<TarefaDto> findAllTarefaUser(Integer id);
+    void findBYIdUser(Integer id);
 }
--- back/Metas/src/main/java/com/to_do_list/Metas/service/impl/TarefaServiceImpl.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/service/impl/TarefaServiceImpl.java
@@ -1,9 +1,12 @@
 package com.to_do_list.Metas.service.impl;
 
 
+import com.to_do_list.Metas.model.User;
 import com.to_do_list.Metas.model.dto.TarefaDto;
 import com.to_do_list.Metas.repositorio.TarefaRepositorio;
+import com.to_do_list.Metas.repositorio.UserRepository;
 import com.to_do_list.Metas.service.TarefaService;
+import com.to_do_list.Metas.service.exception.NotFoundUserException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -20,12 +24,19 @@ public class TarefaServiceImpl implements TarefaService {
     private TarefaRepositorio repositorio;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private ModelMapper mapper;
     @Override
     public List<TarefaDto> findAllTarefaUser(Integer id) {
-
-        BeanUtils.copyProperties(repositorio.findById(id).get(),TarefaDto.class);
-
+        //findBYIdUser(id); preciso usar a fk para pesquisar por ela.
         return Collections.singletonList(mapper.map(repositorio.findById(id), TarefaDto.class));
+    }
+
+    @Override
+    public void findBYIdUser(Integer id) {
+        Optional<User> user = userRepository.findById(id);
+        user.orElseThrow(() -> new NotFoundUserException("Usuario não encontrado."));
     }
 }
