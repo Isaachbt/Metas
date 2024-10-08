e5ddf69e2bd2a3490f1d4489ff9f3c60cd9eabea
Isaachbt <isaac.silva1478@gmail.com>
2024-10-08 17:41:38 -0300

    Metodo findAllTarefaUser retornando valores

--- back/Metas/src/main/java/com/to_do_list/Metas/controller/TarefaController.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/controller/TarefaController.java
@@ -1,10 +1,27 @@
 package com.to_do_list.Metas.controller;
 
+import com.to_do_list.Metas.model.dto.TarefaDto;
+import com.to_do_list.Metas.service.impl.TarefaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/tarefas")
 public class TarefaController {
+
+    @Autowired
+    private TarefaServiceImpl service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<List<TarefaDto>> findAllTarefaUser(@PathVariable Integer id){
+
+        return ResponseEntity.ok().body(service.findAllTarefaUser(id));
+
+    }
 }
