4aa88a21607950852547f0c524bea928b3faf15c
Isaachbt <isaac.silva1478@gmail.com>
2024-10-12 15:53:25 -0300

    metodo findByUserId no TarefaRepositorio.java

--- back/Metas/src/main/java/com/to_do_list/Metas/repositorio/TarefaRepositorio.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/repositorio/TarefaRepositorio.java
@@ -4,6 +4,10 @@ import com.to_do_list.Metas.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TarefaRepositorio extends JpaRepository<Tarefa,Integer> {
+    Optional<List<Tarefa>> findByUserId(Integer user_id);
 }
