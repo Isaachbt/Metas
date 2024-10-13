ecb3a1a1f8342267ffa5523fc508a0b35fb90c4a
Isaachbt <isaac.silva1478@gmail.com>
2024-10-13 18:50:10 -0300

    Novo metodo saveTarefa e remove findByIdUser TarefaServiceImpl

--- back/Metas/src/main/java/com/to_do_list/Metas/service/impl/TarefaServiceImpl.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/service/impl/TarefaServiceImpl.java
@@ -8,11 +8,10 @@ import com.to_do_list.Metas.service.TarefaService;
 import com.to_do_list.Metas.service.UserService;
 import com.to_do_list.Metas.service.exception.TarefaNotFoundException;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.BeanUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,16 +27,17 @@ public class TarefaServiceImpl implements TarefaService {
     private ModelMapper mapper;
 
     @Override
-    public List<Tarefa> findAllTarefaUser(Integer id) {
-      Optional<List<Tarefa>> tarefaList = repositorio.findByUserId(userService.findByIdUser(id).getId());
-      tarefaList.orElseThrow(() -> new TarefaNotFoundException("Crie sua primeira tarefa."));
-
-      return tarefaList.get();
-
+    public String saveTatefa(TarefaDto dto) {
+        userService.findByIdUser(dto.getUserId());
+        repositorio.save(mapper.map(dto,Tarefa.class));
+        return "Salvo com sucesso.";
     }
 
     @Override
-    public void findBYIdUser(Integer id) {
+    public List<Tarefa> findAllTarefaUser(Integer id) {
+      Optional<List<Tarefa>> tarefaList = repositorio.findByUserId(userService.findByIdUser(id).getId());
+      tarefaList.orElseThrow(() -> new TarefaNotFoundException("Crie sua primeira tarefa."));
+      return tarefaList.get();
 
     }
 }
