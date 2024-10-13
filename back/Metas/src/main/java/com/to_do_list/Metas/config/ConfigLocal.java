e7aa4af04240b6e7e5fb05a46c3891aa72716f94
Isaachbt <isaac.silva1478@gmail.com>
2024-10-13 18:47:39 -0300

    saveUser e tarefa ConfigLocal.java

--- back/Metas/src/main/java/com/to_do_list/Metas/config/ConfigLocal.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/config/ConfigLocal.java
@@ -2,12 +2,12 @@ package com.to_do_list.Metas.config;
 
 import com.to_do_list.Metas.model.Tarefa;
 import com.to_do_list.Metas.model.User;
+
 import com.to_do_list.Metas.model.role.RoleUser;
 import com.to_do_list.Metas.repositorio.TarefaRepositorio;
 import com.to_do_list.Metas.repositorio.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -20,11 +20,17 @@ public class ConfigLocal {
 
     @Autowired
     private TarefaRepositorio repositorio;
+    @Autowired
+    private UserRepository userRepository;
 
-//    @PostConstruct
-//    public void saveTarefa(){
-//        Tarefa t = new Tarefa(null,"Dormir", LocalDateTime.now(),LocalDateTime.now(),6,52);
+    @PostConstruct
+    public void saveTarefa(){
+//        Tarefa t = new Tarefa(null,"Dormir", LocalDateTime.now(),LocalDateTime.now(),6,1);
 //        Tarefa t2 = new Tarefa(null,"Comer", LocalDateTime.now(),LocalDateTime.now(),7,2);
-//        repositorio.saveAll(List.of(t,t2));
-//    }
+//        User user = new User(null,"Isaac@gmail","123", RoleUser.USER);
+//        User user2 = new User(null,"Net@gmail","123", RoleUser.USER);
+//
+//        userRepository.saveAll(List.of(user,user2));
+        //repositorio.saveAll(List.of(t,t2));
+    }
 }
