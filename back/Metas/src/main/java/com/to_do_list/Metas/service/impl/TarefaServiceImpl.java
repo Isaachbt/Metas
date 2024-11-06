981f8ddf6dcf1b1fa3e27838150ca9edca347b5c
Isaachbt <isaac.silva1478@gmail.com>
2024-11-06 19:22:24 -0300

    Novo metodo Update

--- back/Metas/src/main/java/com/to_do_list/Metas/service/impl/TarefaServiceImpl.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/service/impl/TarefaServiceImpl.java
@@ -6,9 +6,11 @@ import com.to_do_list.Metas.model.dto.TarefaDto;
 import com.to_do_list.Metas.repositorio.TarefaRepositorio;
 import com.to_do_list.Metas.service.TarefaService;
 import com.to_do_list.Metas.service.UserService;
+import com.to_do_list.Metas.service.exception.NotFoundUserException;
 import com.to_do_list.Metas.service.exception.TarefaNotFoundException;
 import org.modelmapper.ModelMapper;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,6 +40,20 @@ public class TarefaServiceImpl implements TarefaService {
       Optional<List<Tarefa>> tarefaList = repositorio.findByUserId(userService.findByIdUser(id).getId());
       tarefaList.orElseThrow(() -> new TarefaNotFoundException("Crie sua primeira tarefa."));
       return tarefaList.get();
+    }
 
+    @Override
+    public void updateTarefa(TarefaDto dto) throws TarefaNotFoundException,NotFoundUserException{
+        try {
+                repositorio.findById(dto.getId()).orElseThrow(() -> new TarefaNotFoundException("Id tarefa invalido."));
+                repositorio.findByUserId(dto.getUserId()).orElseThrow(() -> new NotFoundUserException("Id usuario não existe."));
+
+                var tarefa = new Tarefa();
+                BeanUtils.copyProperties(dto,tarefa);
+                repositorio.save(tarefa);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
