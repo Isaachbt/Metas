746fdfd15833ddb5eabe034e7c224032e984954c
Isaachbt <isaac.silva1478@gmail.com>
2024-11-15 19:35:30 -0300

    new UserLoginDTO

---
+++ back/Metas/src/main/java/com/to_do_list/Metas/model/dto/UserLoginDTO.java
@@ -0,0 +1,14 @@
+package com.to_do_list.Metas.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginDTO {
+    @NotBlank
+    private String login;
+    @NotBlank
+    private String password;
+}
