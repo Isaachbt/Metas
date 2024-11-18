db3c6a337c1ec1ff32a26a2fdcb4cf6d24afa8d2
Isaachbt <isaac.silva1478@gmail.com>
2024-11-17 22:07:36 -0300

    tratando erro 403 and 401, cada um personalizado, class customizada para tratar esses erros personalizados

--- back/Metas/src/main/java/com/to_do_list/Metas/config/ConfigSecurity.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/config/ConfigSecurity.java
@@ -1,6 +1,7 @@
 package com.to_do_list.Metas.config;
 
 import com.to_do_list.Metas.controller.exceptions.CustomAccessDeniedHandler;
+import com.to_do_list.Metas.controller.exceptions.CustomAuthenticationEntryPoint;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -25,7 +26,13 @@ public class ConfigSecurity {
     private SecurityFilter securityFilter;
 
     @Autowired
-    private CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAccessDeniedHandler error403;
+    @Autowired
+    private CustomAuthenticationEntryPoint error401;
+
+    public ConfigSecurity(CustomAccessDeniedHandler accessDeniedHandler) {
+        this.error403 = accessDeniedHandler;
+    }
 
     @SneakyThrows
     @Bean
@@ -34,14 +41,16 @@ public class ConfigSecurity {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/auth/perfil").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/auth/perfil").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/tarefas/findAll").hasRole("USER")
                         .requestMatchers(HttpMethod.POST, "/tarefas/save-tarefa").hasRole("USER")
                         .requestMatchers(HttpMethod.PUT, "/tarefa/update-tarefa").hasRole("USER")
                         .requestMatchers(HttpMethod.DELETE, "/tarefa/delete").hasRole("USER")
                         .anyRequest().authenticated())
+                .exceptionHandling(e ->e.accessDeniedHandler(error403)
+                        .authenticationEntryPoint(error401))
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(exceptions -> exceptions.accessDeniedHandler(accessDeniedHandler))
+                .exceptionHandling(exceptions -> exceptions.accessDeniedHandler(error403))
                 .build();
     }
 
