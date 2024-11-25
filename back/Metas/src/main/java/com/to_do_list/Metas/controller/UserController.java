e50900324910c5866d7ba6507ddf46ac030fb9b3
Isaachbt <isaac.silva1478@gmail.com>
2024-11-25 18:49:29 -0300

    add restController

--- back/Metas/src/main/java/com/to_do_list/Metas/controller/UserController.java
+++ back/Metas/src/main/java/com/to_do_list/Metas/controller/UserController.java
@@ -18,14 +18,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @Controller
+@RestController
 @RequestMapping("/auth")
 public class UserController {
     @Autowired
